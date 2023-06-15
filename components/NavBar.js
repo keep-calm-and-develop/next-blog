117f906f72c335469af2366f16967b38983a9bee
Aniket <aniket@nirmata.com>
2023-06-15 12:44:46 +0530

    Add styled jsx for NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,6 +11,20 @@ function NavBar() {
                     <Link href='/about'>About</Link>
                 </li>
             </ul>
+            <style jsx>{`
+                ul {
+                    list-style-type: none;
+                    padding: 0;
+                }
+
+                li {
+                    display: inline;
+                }
+                
+                li:not(:first-child) {
+                    margin-left: 0.75rem;
+                }
+            `}</style>
         </nav>
     );
 }
